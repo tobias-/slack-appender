df353e67e0677305f1068b91769f46cf11a3c720
Tobias <tobias@olsson.be>
2015-10-20 21:47:38 +0200

    Add "test" case that does a logging

---
+++ src/test/java/be/olsson/slackappender/SlackAppenderIT.java
@@ -0,0 +1,35 @@
+package be.olsson.slackappender;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Enumeration;
+
+@SuppressWarnings("ALL")
+public class SlackAppenderIT {
+    public static final String SLACK_WEBHOOK = "SLACK_WEBHOOK";
+
+    @Before
+    public void setup() {
+        String webhookUrl = System.getProperty(SLACK_WEBHOOK, System.getenv(SLACK_WEBHOOK));
+        setWebhookUrl(webhookUrl);
+    }
+
+    public void setWebhookUrl(String webhookUrl) {
+        Logger logger = Logger.getRootLogger();
+        for (Enumeration appenders = logger.getAllAppenders(); appenders.hasMoreElements(); ) {
+            Appender appender = (Appender) appenders.nextElement();
+            if (appender instanceof SlackAppender) {
+                ((SlackAppender)appender).setWebhookUrl(webhookUrl);
+            }
+        }
+    }
+
+    @Test
+    public void sendInfo() throws InterruptedException {
+        Logger.getLogger(getClass()).warn("Test warning", new Throwable("This is a test exception"));
+        Thread.sleep(1000);
+    }
+}
